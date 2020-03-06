0547a6985750bc61ec456cb93c8b48429111a32f
Sinoa <sinoa.dev@gmail.com>
2020-03-06 18:54:43 +0900

    [WIP]Application

---
+++ DiscordApplication/Application.cs
@@ -0,0 +1,73 @@
+﻿// zlib/libpng License
+//
+// Copyright(c) 2020 Sinoa
+//
+// This software is provided 'as-is', without any express or implied warranty.
+// In no event will the authors be held liable for any damages arising from the use of this software.
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it freely,
+// subject to the following restrictions:
+//
+// 1. The origin of this software must not be misrepresented; you must not claim that you wrote the original software.
+//    If you use this software in a product, an acknowledgment in the product documentation would be appreciated but is not required.
+// 2. Altered source versions must be plainly marked as such, and must not be misrepresented as being the original software.
+// 3. This notice may not be removed or altered from any source distribution.
+
+using System.Threading;
+using System.Threading.Tasks;
+using Discord.WebSocket;
+
+namespace DiscordApplication
+{
+    public abstract class Application
+    {
+        private readonly DiscordSocketClient client;
+        private readonly SynchronizationContext synchronizationContext;
+
+
+
+        public Application()
+        {
+            client = new DiscordSocketClient();
+            client.LoggedIn += Client_LoggedIn;
+            client.LoggedOut += Client_LoggedOut;
+            client.MessageReceived += Client_MessageReceived;
+        }
+
+
+        private Task Client_LoggedIn()
+        {
+            synchronizationContext.Send(x => OnLoggedIn(), null);
+            return Task.CompletedTask;
+        }
+
+
+        private Task Client_LoggedOut()
+        {
+            synchronizationContext.Send(x => OnLoggedOut(), null);
+            return Task.CompletedTask;
+        }
+
+
+        private Task Client_MessageReceived(SocketMessage arg)
+        {
+            throw new System.NotImplementedException();
+        }
+
+
+        protected virtual string GetBotToken()
+        {
+            return null;
+        }
+
+
+        protected virtual void OnLoggedIn()
+        {
+        }
+
+
+        protected virtual void OnLoggedOut()
+        {
+        }
+    }
+}
---
+++ DiscordApplication/DiscordSynchronizationContext.cs
@@ -0,0 +1,114 @@
+﻿// zlib/libpng License
+//
+// Copyright(c) 2020 Sinoa
+//
+// This software is provided 'as-is', without any express or implied warranty.
+// In no event will the authors be held liable for any damages arising from the use of this software.
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it freely,
+// subject to the following restrictions:
+//
+// 1. The origin of this software must not be misrepresented; you must not claim that you wrote the original software.
+//    If you use this software in a product, an acknowledgment in the product documentation would be appreciated but is not required.
+// 2. Altered source versions must be plainly marked as such, and must not be misrepresented as being the original software.
+// 3. This notice may not be removed or altered from any source distribution.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Threading;
+
+namespace DiscordApplication
+{
+    internal sealed class DiscordSynchronizationContext : SynchronizationContext, IDisposable
+    {
+        private readonly int contextThreadID;
+        private readonly Queue<Message> messageQueue;
+        private readonly SynchronizationContext prevContext;
+
+
+
+        public DiscordSynchronizationContext(out Action messagePumpHandler)
+        {
+            prevContext = AsyncOperationManager.SynchronizationContext;
+            contextThreadID = Thread.CurrentThread.ManagedThreadId;
+            messageQueue = new Queue<Message>();
+            messagePumpHandler = DoProcessMessage;
+            AsyncOperationManager.SynchronizationContext = this;
+        }
+
+
+        public void Dispose()
+        {
+            if (AsyncOperationManager.SynchronizationContext == this)
+            {
+                AsyncOperationManager.SynchronizationContext = prevContext;
+            }
+        }
+
+
+        public override void Send(SendOrPostCallback d, object state)
+        {
+            if (Thread.CurrentThread.ManagedThreadId == contextThreadID)
+            {
+                d(state);
+                return;
+            }
+
+
+            using var waitHandle = new ManualResetEvent(false);
+            lock (messageQueue) messageQueue.Enqueue(new Message(d, state, waitHandle));
+            waitHandle.WaitOne();
+        }
+
+
+        public override void Post(SendOrPostCallback d, object state)
+        {
+            lock (messageQueue) messageQueue.Enqueue(new Message(d, state, null));
+        }
+
+
+        private void DoProcessMessage()
+        {
+            lock (messageQueue)
+            {
+                var nowMessageCount = messageQueue.Count;
+                for (int i = 0; i < nowMessageCount; ++i)
+                {
+                    messageQueue.Dequeue().Invoke();
+                }
+            }
+        }
+
+
+
+        private readonly struct Message
+        {
+            private readonly SendOrPostCallback callback;
+            private readonly ManualResetEvent waitHandle;
+            private readonly object state;
+
+
+
+            public Message(SendOrPostCallback callback, object state, ManualResetEvent waitHandle)
+            {
+                this.callback = callback;
+                this.waitHandle = waitHandle;
+                this.state = state;
+            }
+
+
+            public void Invoke()
+            {
+                try
+                {
+                    callback(state);
+                }
+                finally
+                {
+                    waitHandle?.Set();
+                }
+            }
+        }
+    }
+}
