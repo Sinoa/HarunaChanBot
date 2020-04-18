8acc4862dd1e7d7c2851fa93f3fe2d710152fcaa
Sinoa <sinoa.dev@gmail.com>
2020-04-18 23:32:11 +0900

    スタンプバージョンに対応

--- HarunaChanBot/Services/HaipaiService.cs
+++ HarunaChanBot/Services/HaipaiService.cs
@@ -14,6 +14,7 @@
 // 3. This notice may not be removed or altered from any source distribution.
 
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using HarunaChanBot.Framework;
@@ -38,19 +39,29 @@ namespace HarunaChanBot.Services
                 if (!KaiwaParser.ParseMessageCommand(message.Content, out var command, out var arguments)) continue;
                 if (command != "配牌して" && command != "はいぱいして") continue;
 
+                var stampFlag = true;
+                arguments ??= Array.Empty<string>();
+                if (arguments.Length >= 1)
+                {
+                    if (arguments[0] == "文字列として")
+                    {
+                        stampFlag = false;
+                    }
+                }
 
                 var playerData = service.GetPlayerData(message);
                 var deck = Shuffle(CreateDeck(), DsfmtRandom.Create());
                 var haipai = TakePai(deck);
                 Array.Sort(haipai);
                 var buffer = new StringBuilder();
-                foreach (var chara in haipai.Select(x => ToMahjongChara(x)))
+                foreach (var chara in haipai.Select(x => stampFlag ? ToMahjongStamp(x) : ToMahjongChara(x)))
                 {
                     buffer.Append(chara);
                 }
 
 
-                var doraShowPai = ToMahjongChara(deck[deck.Length - 5]);
+                var doraShowID = deck[deck.Length - 5];
+                var doraShowPai = stampFlag ? ToMahjongStamp(doraShowID) : ToMahjongChara(doraShowID);
                 Application.Current.Post.ReplyMessage($"今回の配牌はこれだよ！\nドラ表示牌：{doraShowPai}\n{buffer}", message, message.Channel, playerData.GetMentionSuffixText());
             }
         }
@@ -82,7 +93,7 @@ namespace HarunaChanBot.Services
                 {
                     for (int k = 0; k < 4; ++k)
                     {
-                        var cat = (i & 3) << 4;
+                        var cat = (i & 3) << 4 | ((j == 5 && k == 0) ? 0x40 : 0x00);
                         var num = (j + 1) & 0x0F;
                         deck[i * 36 + j * 4 + k] = (byte)(cat | num);
                     }
@@ -135,6 +146,7 @@ namespace HarunaChanBot.Services
 
         private string ToMahjongChara(byte data)
         {
+            data = (byte)(data & 0xBF);
             char highSurrogate = (char)0xD83C;
             if (0x01 <= data && data <= 0x09)
             {
@@ -180,5 +192,59 @@ namespace HarunaChanBot.Services
 
             return string.Empty;
         }
+
+
+        private string ToMahjongStamp(byte data)
+        {
+            if (stampTable.TryGetValue(data, out var stamp)) return stamp;
+            return string.Empty;
+        }
+
+
+        private Dictionary<byte, string> stampTable = new Dictionary<byte, string>()
+        {
+            { 0x01, "<:49_1M:667121355031969842>" },
+            { 0x02, "<:48_2M:667121375240257556>" },
+            { 0x03, "<:47_3M:667121390163329044>" },
+            { 0x04, "<:46_4M:667121405195976715>" },
+            { 0x05, "<:45_5M:667121422149222412>" },
+            { 0x06, "<:44_6M:667121439807242253>" },
+            { 0x07, "<:43_7M:667121456035135519>" },
+            { 0x08, "<:42_8M:667121470635245620>" },
+            { 0x09, "<:41_9M:667121484627443712>" },
+
+            { 0x11, "<:29_1P:667121058372911108>" },
+            { 0x12, "<:28_2P:667121073682120734>" },
+            { 0x13, "<:27_3P:667121088504922112>" },
+            { 0x14, "<:26_4P:667121102086209547>" },
+            { 0x15, "<:25_5P:667121119605817344>" },
+            { 0x16, "<:24_6P:667121136198483984>" },
+            { 0x17, "<:23_7P:667121153202192405>" },
+            { 0x18, "<:22_8P:667121168053960724>" },
+            { 0x19, "<:21_9P:667121183195398162>" },
+
+            { 0x21, "<:39_1S:667121204888469531>" },
+            { 0x22, "<:38_2S:667121218997977096>" },
+            { 0x23, "<:37_3S:667121235112624163>" },
+            { 0x24, "<:36_4S:667121252078714890>" },
+            { 0x25, "<:35_5S:667121273998016553>" },
+            { 0x26, "<:34_6S:667121290452140042>" },
+            { 0x27, "<:33_7S:667121305069289498>" },
+            { 0x28, "<:32_8S:667121320991129640>" },
+            { 0x29, "<:31_9S:667121338992820234>" },
+
+            { 0x30, "<:57_1Z:667120916043530250>" },
+            { 0x31, "<:56_2Z:667120937572892692>" },
+            { 0x32, "<:55_3Z:667120949308686376>" },
+            { 0x33, "<:54_4Z:667120961211990067>" },
+
+            { 0x34, "<:53_5Z:667120979511607316>" },
+            { 0x35, "<:52_6Z:667120999380025354>" },
+            { 0x36, "<:51_7Z:667121017298223132>" },
+
+            { 0x45, "<:53_5Z:667120979511607316>" },
+            { 0x55, "<:52_6Z:667120999380025354>" },
+            { 0x65, "<:51_7Z:667121017298223132>" },
+        };
     }
 }
