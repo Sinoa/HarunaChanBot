f44a271df4f2452f6ad8dbcf235a34d7ce7d8ec5
Sinoa <sinoa.dev@gmail.com>
2020-06-04 04:13:22 +0900

    Linux向け実装

--- HarunaChanBot/Services/SystemControlService.cs
+++ HarunaChanBot/Services/SystemControlService.cs
@@ -24,16 +24,7 @@ namespace HarunaChanBot.Services
     public class SystemControlService : ApplicationService
     {
         private double maxFrameNanoTime;
-        private System.Windows.Forms.PowerLineStatus lastPowerLineStatus;
-        private ISocketMessageChannel channel;
-        private bool notifyed;
 
-
-
-        public SystemControlService()
-        {
-            lastPowerLineStatus = System.Windows.Forms.SystemInformation.PowerStatus.PowerLineStatus;
-        }
 
 
         protected internal override void Update()
@@ -61,76 +52,11 @@ namespace HarunaChanBot.Services
                         break;
 
 
-                    case "システム通知はここにお願い":
-                        SetChannel(message, playerData);
-                        break;
-
-
                     case "家に帰って":
                         Logout(message, playerData);
                         break;
                 }
             }
-
-
-            UpdatePowerStatus();
-        }
-
-
-        private void UpdatePowerStatus()
-        {
-            if (channel == null)
-            {
-                return;
-            }
-
-
-            var currentPowerLineStatus = System.Windows.Forms.SystemInformation.PowerStatus.PowerLineStatus;
-            if (lastPowerLineStatus != currentPowerLineStatus)
-            {
-                lastPowerLineStatus = currentPowerLineStatus;
-                switch (lastPowerLineStatus)
-                {
-                    case System.Windows.Forms.PowerLineStatus.Unknown:
-                    case System.Windows.Forms.PowerLineStatus.Online:
-                        Application.Current.Post.SendMessage($"<@{Application.Current.SupervisorID}>ご飯が出来たよ！", channel);
-                        break;
-
-
-                    default:
-                    case System.Windows.Forms.PowerLineStatus.Offline:
-                        Application.Current.Post.SendMessage($"<@{Application.Current.SupervisorID}>ご飯がもう無いよ！", channel);
-                        break;
-                }
-
-
-                notifyed = false;
-            }
-
-
-            if (currentPowerLineStatus == System.Windows.Forms.PowerLineStatus.Offline)
-            {
-                var currentPowerLevel = System.Windows.Forms.SystemInformation.PowerStatus.BatteryLifePercent;
-                if (notifyed == false && currentPowerLevel <= 0.15f)
-                {
-                    Application.Current.Post.SendMessage($"<@{Application.Current.SupervisorID}>うぅ。。。お腹すいたぁ、陽菜ご飯を食べたいよ～", channel);
-                    notifyed = true;
-                }
-            }
-        }
-
-
-        private void SetChannel(SocketMessage message, PlayerGameData playerData)
-        {
-            if (message.Author.Id != Application.Current.SupervisorID)
-            {
-                Application.Current.Post.ReplyMessage("ごめんなさい、知らない人の言葉を信じちゃいけないってお母さんから言われているの。", message, message.Channel, playerData.GetMentionSuffixText());
-                return;
-            }
-
-
-            channel = message.Channel;
-            Application.Current.Post.ReplyMessage($"何かあったらここに連絡するね！", message, message.Channel, playerData.GetMentionSuffixText());
         }
 
 
@@ -155,22 +81,16 @@ namespace HarunaChanBot.Services
 
         private void ShowSystemInformation(SocketMessage message)
         {
-            var powerStatus = System.Windows.Forms.SystemInformation.PowerStatus;
-            var remaining = TimeSpan.FromSeconds(powerStatus.BatteryLifeRemaining);
             var messageText = $@"
 現在稼働中のシステムのステータスを開示します。
-フレーム処理時間：{(Application.Current.FrameNanoTime / 1000000.0).ToString("N3")} ms（{(1.0 / Application.Current.FrameNanoTime * 1000000000.0).ToString("N0")} FPS）
-最大フレーム処理時間：{(maxFrameNanoTime / 1000000.0).ToString("N3")} ms（{(1.0 / maxFrameNanoTime * 1000000000.0).ToString("N0")} FPS）
-メモリ使用量：{Environment.WorkingSet.ToString("N0")} Bytes
-GC使用量：{GC.GetTotalMemory(false).ToString("N0")} Bytes
+フレーム処理時間：{Application.Current.FrameNanoTime / 1000000.0:N3} ms（{1.0 / Application.Current.FrameNanoTime * 1000000000.0:N0} FPS）
+最大フレーム処理時間：{maxFrameNanoTime / 1000000.0:N3} ms（{1.0 / maxFrameNanoTime * 1000000000.0:N0} FPS）
+メモリ使用量：{Environment.WorkingSet:N0} Bytes
+GC使用量：{GC.GetTotalMemory(false):N0} Bytes
 GC世代数：{GC.MaxGeneration + 1} 世代
 GCカウント(世代0)：{GC.CollectionCount(0)} 回
 GCカウント(世代1)：{GC.CollectionCount(1)} 回
-GCカウント(世代2)：{GC.CollectionCount(2)} 回
-電源接続状況：{powerStatus.PowerLineStatus}
-電池充電状態：{powerStatus.BatteryChargeStatus}
-電池充電割合：{(int)(powerStatus.BatteryLifePercent * 100)} %
-電池充電時間：{TimeSpan.FromSeconds(powerStatus.BatteryLifeRemaining)}";
+GCカウント(世代2)：{GC.CollectionCount(2)} 回";
             Application.Current.Post.ReplyMessage(messageText, message);
         }
 
